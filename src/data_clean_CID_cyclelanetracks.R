098ba2b131c348a3f9de83bbb565f8b378ecd398
PublicHealthDataGeek <cjtaituk@yahoo.co.uk>
2021-02-22 11:18:30 +0000

    finalised cleansing code for cycle lanes and tracks, new code written for compiling borough level datasets

--- src/data_clean_CID_cyclelanetracks.R
+++ src/data_clean_CID_cyclelanetracks.R
@@ -128,7 +128,7 @@ f_cycle_lane_track$CLT_MANDAT = fct_recode(f_cycle_lane_track$CLT_MANDAT, "TRUE"
 # When mapped, it can been seen that some cycle lanes/tracks that are coloured
 # ie have BOROUGH assigned actually cross into other Boroughs.  Most obvious example
 # is a yellow track in Merton (near Wimbledon)
-mapview(f_cycle_lane_track, zcol = "BOROUGH", na.color = "red") + 
+pre_borough_cleanse_map = mapview(f_cycle_lane_track, zcol = "BOROUGH", na.color = "red") + 
   mapview(lon_lad_2020, alpha.regions = 0.1, legend = FALSE, lwd = 1)
 ## NB above map - Westminster legend is coloured red but its lines are a yellow. 
 ## Will need sorting if publishing.  
@@ -241,7 +241,7 @@ lanes_borough_corrected = new_segments_corrected %>%
   st_cast("MULTILINESTRING") %>%
   mutate(BOROUGH = as.character(BOROUGH)) # change borough to character so can match back to f_cycle_lane_track
 
-# Remove duplicates from f_cycle_lane_track
+# Remove the 72 observations from f_cycle_lane_track that have been replaced by the 144 new observations
 f_cycle_lane_track = f_cycle_lane_track %>%
   filter(!FEATURE_ID %in% multi_feature_id_list) # n = 24904 ie 24976 - 72 THis is correct
 
@@ -249,7 +249,7 @@ f_cycle_lane_track = f_cycle_lane_track %>%
 f_cycle_lane_track = rbind(f_cycle_lane_track, lanes_borough_corrected) # n= 25048
 
 # If visualise again, now looks correct - just need to sort out observations with BOROUGH NAs
-mapview(f_cycle_lane_track, zcol = "BOROUGH", na.color = "red") + 
+mid_borough_cleanse_map = mapview(f_cycle_lane_track, zcol = "BOROUGH", na.color = "red") + 
   mapview(lon_lad_2020, alpha.regions = 0.1, legend = FALSE, lwd = 1)
 
 
@@ -259,8 +259,9 @@ mapview(f_cycle_lane_track, zcol = "BOROUGH", na.color = "red") +
 anyNA(f_cycle_lane_track$BOROUGH) # = TRUE
 
 # 1) identify missing Borough details
-cycle_lane_borough_NA = f_cycle_lane_track %>%
-  filter(is.na(BOROUGH)) # 354 observations ie 354 cycle lanes/tracks have no Borough
+# cycle_lane_borough_NA = f_cycle_lane_track %>%
+#    filter(is.na(BOROUGH)) # 354 observations ie 354 cycle lanes/tracks have no Borough
+# already created above
 
 # 2) Split each observation into segments using ONS borough boundaries 
 lanes_borough_NA_i = st_intersection(lon_lad_2020, cycle_lane_borough_NA) # 621 observations
@@ -268,10 +269,11 @@ lanes_borough_NA_i = st_intersection(lon_lad_2020, cycle_lane_borough_NA) # 621 
 # cycle lane segments outside London boroughs have been dropped
 # this has broken each unique FEATURE_ID into segments based on whether they cross a borough line 
 # NB each segment may contain more than one geometry type
-summary(lanes_borough_NA_i$geometry) # -> 519 linestrings and 102 multilinestrings
+
+# summary(lanes_borough_NA_i$geometry) # -> 519 linestrings and 102 multilinestrings
 lanes_borough_NA_i = lanes_borough_NA_i %>%
   st_cast("MULTILINESTRING") # convert geometry type to MLS for all so can mapview
-summary(lanes_borough_NA_i$geometry) # -> 621 MLS.  Now can mapview(lanes_borough_NA_i)
+# summary(lanes_borough_NA_i$geometry) # -> 621 MLS.  Now can mapview(lanes_borough_NA_i)
 
 cycle_lanes_NA_map = mapview(lanes_borough_NA_i, zcol = "BOROUGH") + mapview(lon_lad_2020, alpha.regions = 0.1, zcol = "BOROUGH", legend = FALSE)
 
@@ -287,7 +289,7 @@ count_obs = lanes_borough_NA_i %>%
 #     1    88   # 88 feature_ids have 1 observation  NB observation may contain multiple lines but all have same characteristics
 #     2   265
 #     3     1
-# so 88 are unique so ok but the ones with 2 and 3 need recoding with separate FEATURE_IDs
+
 
 # 4) Relabel FEATURE_ID so each segment can be identified
 # Group 621 by FEATURE_ID then add a cumulative sum to each grouped observation (will be 1, 2 or 3)
@@ -318,10 +320,7 @@ lanes_borough_NA_corrected = lanes_borough_NA_corrected %>%
            "CLT_WATERR", "CLT_PTIME",  "CLT_ACCESS", "CLT_COLOUR", "BOROUGH", 
            "PHOTO1_URL", "PHOTO2_URL", "geometry")) %>%
   mutate(BOROUGH = as.character(BOROUGH)) # change borough to character so can match back to f_cycle_lane_track
-unique(st_geometry_type(crossings_borough_NA_corrected)) # Linestring
-crossings_borough_NA_corrected = crossings_borough_NA_corrected %>%
-  st_cast("MULTILINESTRING") %>%
-  mutate(BOROUGH = as.character(BOROUGH)) # and unfactor BOROUGH for step 6
+
 # NB geometry is already Multilinestring so no need to convert
 
 # 7) Validate that have corrected NAs so count NAs before transformation
@@ -333,12 +332,12 @@ count_lanes_borough = f_cycle_lane_track %>%
 # 8) Join main lanes dataset to corrected borough dataset
 #  8a) drop obs with no boroughs from f_cycle_lane_track
 f_cycle_lane_track = f_cycle_lane_track %>%
-  filter(!is.na(BOROUGH)) # 24622 observations ie  the 24976 - 354 NAs
-anyNA(f_cycle_lane_track$BOROUGH) # = FALSE ie all dropped
+  filter(!is.na(BOROUGH)) # 24694 observations ie  the 25048 - 354 NAs
+#anyNA(f_cycle_lane_track$BOROUGH) # = FALSE ie all dropped
 
 #  8b) join corrected observations to the f_cycle_lane_track
 f_cycle_lane_track = rbind(f_cycle_lane_track, lanes_borough_NA_corrected) 
-anyNA(f_cycle_lane_track$BOROUGH) # = FALSE
+#anyNA(f_cycle_lane_track$BOROUGH) # = FALSE
 
 # 9) Validate have correctly transformed
 #  9a) Recount Boroughs after transformation
@@ -361,127 +360,22 @@ lanes_boroughs = left_join(count_lanes_borough, number_recoded) %>%
 x = lanes_boroughs %>%
   replace_na(list(Count = 0, Number_recoded = 0, Recount = 0))
 total <- sapply(x[,2:4], sum) # this gives figures of:
-# 24976 total count
+# 25048 total count
 # 621 observation recoded 
-# new total number of observations in the lanes dataset of 25243 
-# 25243 = 24976 (original number) - 354 (no borough) + 621 (recoded)
+# new total number of observations in the lanes dataset of 25315 
+# 25315 = 24976 (original number) - 354 (no borough (NA)) + 621 (recoded) + 72 (from incorrect Borough)
 
 
 # 10) Visually inspect cycle lanes and tracks to check now coded correct Borough
-mapview(lon_lad_2020, alpha.regions = 0.1, lwd = 1) + 
+post_borough_cleanse_map = mapview(lon_lad_2020, alpha.regions = 0.1, lwd = 1) + 
   mapview(f_cycle_lane_track, zcol = "BOROUGH") 
-# there appears to be a cycle lane/track that is yellow (Wandsworth) that is in the Boundaries of Merton
 
-wandsworth = f_cycle_lane_track %>%
-  filter(BOROUGH == "Wandsworth")
-wandsworth_map = mapview(wandsworth, zcol = "BOROUGH") + mapview(lon_lad_2020, alpha.regions = 0.1, lwd = 1)
-leafem::addStaticLabels(m1, label = wandsworth$FEATURE_ID) # plot mNA with the FEATURE_IDs labelled so can identify
+######################
+# SAVE CLEAN DATASET #
+######################
+saveRDS(f_cycle_lane_track, file = "/home/bananafan/Documents/PhD/Paper1/data/cleansed_cycle_lane_track")
 
-
-
-
+# leafsync::sync(pre_borough_cleanse_map, mid_borough_cleanse_map, cycle_lanes_NA_map, post_borough_cleanse_map)
+# this map isnt quite right
 
 
-
-# Borough level analysis
-#####
-# Count number of cycle lanes by Borough
-cycle_lane_track_borough_count = f_cycle_lane_track %>%
-  st_drop_geometry() %>%
-  group_by(BOROUGH) %>%
-  summarise(CycleLanesAndTracks = n()) # 33 boroughs no NAs
-
-# Length of cycle lanes and tracks by Borough
-f_cycle_lane_track$length_m = st_length(f_cycle_lane_track$geometry)
-f_cycle_lane_track$length_km = set_units(st_length(f_cycle_lane_track$geometry), km)
-
-cycle_lane_track_borough_length = f_cycle_lane_track %>%
-  group_by(BOROUGH) %>%
-  summarise(across(c(length_m, length_km), sum))
-# drop geometry cant be done within the pipe as it causes weirdness in the measurements (see issue 1)
-# it needs to be dropped to make dataset manageable
-cycle_lane_track_borough_length = st_drop_geometry(cycle_lane_track_borough_length) 
-
-
-# length of cycle lanes and tracks by on/off road by Borough ???
-# ??? other questions
-
-# create new df without geometry that enables faster analysis of data
-non_geom_f_cycle_lane_track = st_drop_geometry(f_cycle_lane_track)
-str(non_geom_f_cycle_lane_track)
-
-
-
-###############################################################################
-##############################
-all_cycle_lanes_split = st_intersection(lon_lad_2020, f_cycle_lane_track) #25315
-#nb original no of obs 24976
-# so 339 extra obs
-# BOROUGH is from ONS, BOROUGH.1 is from CID
-cl_NA_b = all_cycle_lanes_split%>%
-  filter(is.na(BOROUGH.1)) # = 621
-cl_not_NA = all_cycle_lanes_split%>%
-  filter(!is.na(BOROUGH.1)) # = 24694
-anyNA(all_cycle_lanes_split$BOROUGH) # = FALSE - so all have a BOROUGH from ONS
-
-#create smaller dataset Wandsworth according to CID
-wandsworth = all_cycle_lanes_split %>%
-  filter(BOROUGH.1 == "Wandsworth") # n= 974 
-anyNA(wandsworth$BOROUGH.1) # = FALSE so all have Wandsworth from 
-
-unique(wandsworth$BOROUGH.1) # so just wandsworth in the CID 
-#[1] Wandsworth
-
-unique(wandsworth$BOROUGH) # so even though CID labels as Wandsworth, ONS labels 
-# as something else
-#[1] Wandsworth           Merton              
-#[3] Richmond upon Thames Lambeth             
-#[5] Hammersmith & Fulham
-
-wandsworth %>%
-  group_by(BOROUGH) %>%
-  count()  # counts how many 'Wandsworth' CID obs are labelled as other Boroughs by ONS data
-# hammersmith 1
-# lambeth     3
-# merton      1
-# Richmond    1
-# wandworth     968
-
-# check by comparing BOROUGH to BOROUGH.1
-compare_pre = wandsworth$BOROUGH %in% wandsworth$BOROUGH.1
-summary(compare_pre)
-#   Mode   FALSE    TRUE 
-# logical       6     968  # false fits with the 6 from above
-
-# Need to factor BOROUGH.1 
-borough_levels = c("Barking & Dagenham", "Barnet", "Bexley", "Brent",  
-                   "Bromley", "Camden", "City of London", "Croydon", 
-                   "Ealing", "Enfield", "Greenwich", "Hackney",  
-                   "Hammersmith & Fulham", "Haringey", "Harrow", 
-                   "Havering", "Hillingdon", "Hounslow", "Islington", 
-                   "Kensington & Chelsea", "Kingston upon Thames",  
-                   "Lambeth", "Lewisham", "Merton", "Newham", 
-                   "Redbridge", "Richmond upon Thames", "Southwark",  
-                   "Sutton", "Tower Hamlets", "Waltham Forest",   
-                   "Wandsworth", "Westminster") 
-wandsworth$BOROUGH.1 = factor(wandsworth$BOROUGH.1, levels = borough_levels)
-levels(wandsworth$BOROUGH.1) # check has worked
-
-# create loop to run through and recode 
-for (i in seq_along(wandsworth$BOROUGH)) {
-  wandsworth$BOROUGH.1[[i]] = wandsworth$BOROUGH[[i]]
-}
-
-wandsworth %>%
-  group_by(BOROUGH.1) %>%
-  count()  # counts to check we have the right number recoded 
-# hammersmith 1
-# lambeth     3
-# merton      1
-# Richmond    1
-# wandsworth    968  YES IT WORKS!
-
-compare_post = wandsworth$BOROUGH %in% wandsworth$BOROUGH.1
-summary(compare_post) # all match
-
-##############################################################################
--- src/get_borough_level_datasets.R
+++ src/get_borough_level_datasets.R
@@ -1,224 +1,65 @@
-#############################################
-# Create Borough level comparison data sets #
-#
-# Created 13/1/21
-#
-# This code created datasets of borough level asset data for counts and length
-# that can be used to compare boroughs
-#
-###########
-
-
-#
+##################################################################################
+#  Borough level analysis using cleansed data                                    #
+#                                                                                #
+# This script using the cleansed CID data where the Boroughs have been corrected #  
+# to develop datasets for Borough level analysis                                 #
+#                                                                                #
+##################################################################################
 
 # install packages
 library(tidyverse)
-library(CycleInfraLnd)
 library(sf)
-#library(tmap)
 library(mapview)
-library(leaflet)
-library(leafem)
-#library(leafsync)
-library(summarytools)
-library(forcats)
-library(units)
+# library(leaflet)
+# library(leafem)
+# # library(leafsync)
+# # library(summarytools)
+# library(forcats)
+# library(units)
+#library(tmap)
+#library(CycleInfraLnd)
+
+# Load datasets
+
+cleansed_cycle_lane_track = readRDS(file = "/home/bananafan/Documents/PhD/Paper1/data/cleansed_cycle_lane_track")
 
 
-#######################
-# Advanced Stop Lines #
-#######################
 
-# download CID data and transform CRS
-advanced_stop_line = get_cid_lines(type = "advanced_stop_line")
+# Create Borough level datasets NB ALL THIS CODE NEED CHECKING
 
-# Count number of ASLs by Borough
-asl_borough_count= advanced_stop_line %>%
-  st_drop_geometry() %>%
-  group_by(BOROUGH) %>%
-  summarise(ASL = n()) # 33 boroughs plus 1 asl with no borough assigned
-
-#############
-# Crossings #
-#############
-
-# download crossings data
-crossings = get_cid_lines(type = "crossing")
-
-# Count number of Crossings by Borough
-crossings_borough_count = crossings %>%
-  st_drop_geometry() %>%
-  group_by(BOROUGH) %>%
-  summarise(Crossings = n()) # 33 boroughs plus 28 crossings with no borough
-
-
-##########################
-# Cycle Lanes and Tracks #
-##########################
-
-# download CID data using the Cycle Infra Lnd package
-cycle_lane_track = get_cid_lines(type = "cycle_lane_track") # n = 24976
-cycle_lane_track = st_transform(cycle_lane_track, crs=27700) 
-
-# Count number of cycle lanes by Borough
-cycle_lane_track_borough_count = cycle_lane_track %>%
-  st_drop_geometry() %>%
-  group_by(BOROUGH) %>%
-  summarise(CycleLanesAndTracks = n()) # 33 boroughs plus 354 cycle lanes/tracks with no borough
-
-# Measure Length of cycle lanes and tracks by Borough
-cycle_lane_track$length_m = st_length(cycle_lane_track$geometry)
-cycle_lane_track$length_km = set_units(st_length(cycle_lane_track$geometry), km)
-
-cycle_lane_track_borough_length = cycle_lane_track %>%
-  st_drop_geometry() %>%
-  group_by(BOROUGH) %>%
-  summarise(across(c(length_m, length_km), sum))
-# drop geometry cant be done within the pipe as it causes weirdness in the measurements (see issue 1)
-# it needs to be dropped to make dataset manageable
-cycle_lane_track_borough_length = st_drop_geometry(cycle_lane_track_borough_length) 
-
-#####################
-# Restricted Routes #
-#####################
-
-# download CID data 
-restricted_route = get_cid_lines(type = "restricted_route")
-restricted_route = st_transform(restricted_route, crs=27700) 
-
-# Count number of restricted routes by Borough
-restricted_route_borough_count = restricted_route %>%
-  st_drop_geometry() %>%
-  group_by(BOROUGH) %>%
-  summarise(RestrictedRoutes = n()) # 33 boroughs plus 18 restricted routes with no borough
-
-# Length of restricted routes by Borough
-restricted_route$length_m = st_length(restricted_route$geometry)
-restricted_route$length_km = set_units(st_length(restricted_route$geometry), km)
-
-restricted_route_borough_length = restricted_route %>%
-  group_by(BOROUGH) %>%
-  summarise(across(c(length_m, length_km), sum))
-# drop geometry cant be done within the pipe as it causes weirdness in the measurements (see issue 1)
-# it needs to be dropped to make dataset manageable
-restricted_route_borough_length = st_drop_geometry(restricted_route_borough_length) 
-
-#####################
-# Cycle Parking     #
-#####################
-
-# download data
-cycle_parking = get_cid_points(type = "cycle_parking")
-
-# Count cycle parking locations/sites
-cycle_parking_sites_borough_count = cycle_parking %>%
-  st_drop_geometry() %>%
-  group_by(BOROUGH) %>%
-  summarise(CycleParkingSites = n())
-
-# Count Number of cycling parking spaces (capacity)
-# PRK_PROVIS (number of stands etc) and PRK_CPT (number of bikes that can be 
-# parked) have NAs.  These need correcting before can calculate no of 
-# bikes that can be parked per borough
-# sum(is.na(cycle_parking$PRK_PROVIS)) # 2 NA
-# sum(is.na(cycle_parking$PRK_CPT)) # 2 NAs
-# CPcount_borough_NA = f_cycle_parking %>% # identify which observations are NA
+# ##1) Cycle lanes and tracks
+# # Count number of cycle lanes by Borough
+# cycle_lane_track_borough_count = f_cycle_lane_track %>%
 #   st_drop_geometry() %>%
-#   filter(is.na(PRK_CPT)) # 2 observations have NA - both for PROVIS & CPT
-
-# Asset images reviewed - able to see how many stands and spaces each have
-# RWG999580 4 stands (PROVIS) 8 spaces (CPT)
-# RWG999458 3 stands, 6 spaces
-# therefore can correct missing data
-cycle_parking$PRK_PROVIS[cycle_parking$FEATURE_ID == "RWG999580"] = 4
-cycle_parking$PRK_CPT[cycle_parking$FEATURE_ID == "RWG999580"] = 8
-cycle_parking$PRK_PROVIS[cycle_parking$FEATURE_ID == "RWG999458"] = 3
-cycle_parking$PRK_CPT[cycle_parking$FEATURE_ID == "RWG999458"] = 6
-
-# check coded correctly
-# x = f_cycle_parking %>%
-#   filter(FEATURE_ID == "RWG999580" | FEATURE_ID == "RWG999458") # = yes coded correctly
-# now calculate number of cycle parking spaces by borough
-cycle_parking_spaces_borough_count = cycle_parking %>%
-  st_drop_geometry() %>%
-  group_by(BOROUGH) %>%
-  summarise(CycleParkingSpaces = sum(PRK_CPT))
-
-###########
-# Signals #
-###########
-
-# download data
-signal = get_cid_points(type = "signal") #n = 443
-
-# Count number of signals by Borough 
-signal_borough_count = signal %>%
-  st_drop_geometry() %>%
-  group_by(BOROUGH) %>%
-  summarise(Signals = n())
-
-
-###################
-# Traffic calming #
-###################
-
-# download data
-traffic_calming = get_cid_points(type = "traffic_calming")
-
-# Count number of traffic calming measures by Borough 
-traffic_calming_borough_count = traffic_calming %>%
-  st_drop_geometry() %>%
-  group_by(BOROUGH) %>%
-  summarise(TrafficCalming = n())
-
-###########
-# Signage #
-###########
-
-# download data
-signage = get_cid_points(type = "signage")
-
-# examine RWG999275 as this appears twice
-# RWG_duplicate = signage %>%
-#   filter(FEATURE_ID == "RWG999275") # some difference in variables
-# mapview(RWG_duplicate) # located in different areas
-# create new FEATURE_IDs so that observations can be differentiated
-signage$FEATURE_ID[signage$BOROUGH == "Barnet"] = "RWG999275a"
-signage$FEATURE_ID[signage$BOROUGH == "Haringey"] = "RWG999275b"
-
-# Borough level analysis
-signage_borough_count = signage %>%
-  st_drop_geometry() %>%
-  group_by(BOROUGH) %>%
-  summarise(Signage = n())
-
-#####################
-# Restricted points #
-#####################
-
-# download CID data using the Cycle Infra Lnd package
-restricted_points = get_cid_points(type = "restricted_point")
-restricted_points = st_transform(restricted_points, crs=27700) 
-
-# Borough level analysis
-restricted_points_borough_count = restricted_points %>%
-  st_drop_geometry() %>%
-  group_by(BOROUGH) %>%
-  summarise(RestrictedPoints = n())
-
-##################################################################################
-# Create data frames for Borough level analysis of assets by type
-#
+#   group_by(BOROUGH) %>%
+#   summarise(CycleLanesAndTracks = n()) # 33 boroughs no NAs
 # 
+# # Length of cycle lanes and tracks by Borough
+# f_cycle_lane_track$length_m = st_length(f_cycle_lane_track$geometry)
+# f_cycle_lane_track$length_km = set_units(st_length(f_cycle_lane_track$geometry), km)
 # 
+# cycle_lane_track_borough_length = f_cycle_lane_track %>%
+#   group_by(BOROUGH) %>%
+#   summarise(across(c(length_m, length_km), sum))
+# # drop geometry cant be done within the pipe as it causes weirdness in the measurements (see issue 1)
+# # it needs to be dropped to make dataset manageable
+# cycle_lane_track_borough_length = st_drop_geometry(cycle_lane_track_borough_length) 
+# 
+# # Save these datasets for combination
+
+
+
+
+
+##########################################
+# OLD CODE BELOW - probably still useful - needs checking and correcting though
 
 ####### Count of assets by Borough
-CID_by_borough = list(asl_borough_count, 
-                      crossings_borough_count, 
+CID_by_borough = list(asl_borough_count, crossings_borough_count, 
                       cycle_lane_track_borough_count, 
                       restricted_route_borough_count,
                       cycle_parking_sites_borough_count,
-                      cycle_parking_spaces_borough_count,
+                      cycle_parking_space_borough_count
                       signal_borough_count,
                       traffic_calming_borough_count,
                       signage_borough_count,
@@ -226,7 +67,7 @@ CID_by_borough = list(asl_borough_count,
   reduce(left_join, by = "BOROUGH")
 
 # need to replace NA in Borough with 'Borough not stated' and NAs in other columns with 0
-sum(is.na(CID_by_borough)) # = 22
+sum(is.na(CID_by_borough)) # = 21
 
 # Replace 'Borough NA' with text and count NAs with 0
 CID_by_borough = CID_by_borough %>% 
