250b034d61df88d631f9cfd11536412f3111a41e
PublicHealthDataGeek <cjtaituk@yahoo.co.uk>
2021-03-29 18:19:42 +0100

    updated pct calculations with st_intersections andadded to methods file

--- src/get_pct_km_cycled.R
+++ src/get_pct_km_cycled.R
@@ -12,12 +12,17 @@
 library(tidyverse)
 library(pct)
 library(sf)
+library(mapview)
+mapviewOptions(native.crs = TRUE, legend = FALSE) # set mapview options so that matches crs
 
 # import May 2020 ONS LA boundary data
 lon_lad_2020 = readRDS(file = "./map_data/lon_LAD_boundaries_May_2020_BFE.Rds")
 
-# Get route network for London
-lon_rnet = pct::get_pct_rnet(region = "london")
+# Create Outer London Spatial object for boundary
+out_lon_union = st_union(lon_lad_2020)
+
+# Get route network for London from PCT
+lon_rnet = pct::get_pct_rnet(region = "london") # n = 74624
 
 # Change CRS to ONS CRS
 st_crs(lon_rnet) # QGS 84, ePSG 4326
@@ -29,21 +34,230 @@ names(lon_rnet)
 # [4] "govnearmkt_slc" "gendereq_slc"   "dutch_slc"     
 # [7] "ebike_slc"      "geometry" 
 
-#Calculate road length
-lon_rnet$segment_length = as.numeric(sf::st_length(lon_rnet))
+# Visualise pct data with London Borough boundaries -> some PCT data is outside London Boroughs
+mapview(lon_rnet, color = "red") + mapview(lon_lad_2020, zcol = "BOROUGH")
 
-# summary(lon_rnet$segment_length)
-# Min.   1st Qu.    Median      Mean   3rd Qu.      Max. 
-# 0.145    44.753   103.081   170.911   207.936 11316.022 
+# # Limit PCT data to Outer London Borough Boundaries
+# only_lon_rnet = st_intersection(lon_rnet, out_lon_union) # n = 69872 observations
+# mapview(only_lon_rnet, color = "red") + mapview(lon_lad_2020, zcol = "BOROUGH") # Now all in London Boroughs
+# 
+# # Calculate segment length
+# only_lon_rnet$segment_length = as.numeric(sf::st_length(only_lon_rnet))
 
-# summary(lon_rnet$bicycle)
-# Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
-# 0.00    1.00    6.00   53.33   25.00 4113.00 
+# names(only_lon_rnet)
+# [1] "local_id"       "bicycle"        "govtarget_slc"  "govnearmkt_slc"
+# [5] "gendereq_slc"   "dutch_slc"      "ebike_slc"      "geometry"      
+# [9] "segment_length"
 
-#Calculate daily km's cycled
-# calculate daily metres cycled per day (multiple segment length by number of users)
-lon_rnet$m_cycled_per_working_day = lon_rnet$segment_length * lon_rnet$bicycle
-lon_rnet_centroid = st_centroid(lon_rnet)  # check works - see if any difference with interseciton
+# summary(only_lon_rnet$segment_length)  ie shortest segment is 0.145m, longest is 4691m
+# Min.  1st Qu.   Median     Mean  3rd Qu.     Max. 
+# 0.145   43.452   98.923  148.411  195.906 4691.176 
+
+## For comparison
+## summary(lon_rnet$segment_length)
+## Min.   1st Qu.    Median      Mean   3rd Qu.      Max.
+## 0.145    44.753   103.081   170.911   207.936 11316.022
+
+#  Intersection segments by London Boroughs
+lon_rnet_intersection = st_intersection(lon_lad_2020, only_lon_rnet) # n = 71494 
+# names(lon_rnet_intersection)
+# [1] "BOROUGH"        "objectid"       "lad20cd"        "lad20nmw"      
+# [5] "bng_e"          "bng_n"          "long"           "lat"           
+# [9] "st_areasha"     "st_lengths"     "local_id"       "bicycle"       
+# [13] "govtarget_slc"  "govnearmkt_slc" "gendereq_slc"   "dutch_slc"     
+# [17] "ebike_slc"      "segment_length" "geometry"      
+
+# Find newly created segments by identifying local_id with more than one observation
+multi_local_id_segments = lon_rnet_intersection %>%
+  st_drop_geometry() %>%
+  group_by(local_id) %>%
+  summarise(num_obs = n())%>%
+  group_by(num_obs) %>%
+  filter(num_obs >= 2) %>%
+  summarise(num_groups = n())
+
+# num_obs num_groups
+#  2       1600  ie 1600 have two segments
+#  3         11 have 3 segments
+
+# Create list of local_ids that have 2 or more segments
+multi_feature_id_list = lon_rnet_intersection %>%
+  st_drop_geometry() %>%
+  group_by(local_id) %>%
+  summarise(num_obs = n()) %>%
+  group_by(num_obs) %>%
+  filter(num_obs >= 2) %>%
+  ungroup() %>%
+  pull(local_id)
+
+# Create dataset containing the observations with these local_ids
+new_segments = lon_rnet_intersection %>%
+  filter(local_id %in% multi_feature_id_list) # n = 3233
+# 1600 + 1600 (these segmented into two) 
+# 11 + 11 + 11 (these segmented into 3
+
+# Visualise
+mapview(new_segments, zcol = "BOROUGH") + 
+  mapview(lon_lad_2020, alpha.regions = 0.1, zcol = "BOROUGH", legend = FALSE, lwd = 1)
+
+# Update segment length by inserting new segment length into that column
+new_segments$segment_length = as.numeric(sf::st_length(new_segments))
+sum(new_segments$segment_length) # = 437975.4m (n = 3233) - ie matches that of the original segments with these local_ids
+
+# # Validate this against  the total length of these rnet before intersection
+# x = only_lon_rnet %>%
+#   filter(local_id %in% multi_feature_id_list) # (number of obs is 1611 ie before the split)
+# x$segment_length = as.numeric(sf::st_length(x))
+# sum(x$segment_length) # total length of the new segments should be 437975.4
+
+# calculate total segment length per local_id
+new_segments = new_segments %>%
+  group_by(local_id) %>%
+  mutate(total_segment_length = sum(segment_length)) %>%
+  ungroup()
+
+# obtain proportion of total segment length for each individual segment
+new_segments$length_prop = new_segments$segment_length / new_segments$total_segment_length
+
+# calculate m cycled per working day by multiple number of cyclists by the proportion of length in that segment
+new_segments$m_cycled_per_working_day = new_segments$length_prop * new_segments$bicycle
+
+
+
+# Create dataset containing the observations that dont have multiple id's
+old_segments = lon_rnet_intersection %>%
+  filter(!local_id %in% multi_feature_id_list) # n = 68261 
+
+# add the other columns that the new_segments df contains
+old_segments$segment_length = as.numeric(sf::st_length(old_segments))
+old_segments = old_segments %>%
+  group_by(local_id) %>%
+  mutate(total_segment_length = sum(segment_length)) %>%
+  ungroup()
+old_segments$length_prop = round(old_segments$segment_length / old_segments$total_segment_length) # NB all = 1 ie 100%
+old_segments$m_cycled_per_working_day = old_segments$length_prop * old_segments$bicycle
+
+
+# Join old_segments to new_segments to get full London Borough dataset 
+lon_rnet_pct_cycling_data = rbind(old_segments, new_segments)
+# n = 71494 ? is this correct????  
+
+# Obtain kms cycled for commuting per year (200 = number of working days)
+lon_rnet_pct_cycling_data$km_cycled_for_commuting_per_year_estimated = lon_rnet_pct_cycling_data$m_cycled_per_working_day * 
+  2 * 200 / # estimate of trips days per year, morning and afternoon
+  1000 # to get km
+
+# Create dataset of Boroughs and estimated cycling through the borough
+Borough_commuting = lon_rnet_pct_cycling_data %>%
+  st_drop_geometry() %>%
+  select(c(BOROUGH, km_cycled_for_commuting_per_year_estimated)) %>%
+  group_by(BOROUGH) %>%
+  summarise(total_km_cycled_for_commuting_per_year_estimated = sum(km_cycled_for_commuting_per_year_estimated))
+
+
+
+
+
+
+
+
+# Developing code for the new segments on a smaller dataset
+test_ids = c(74621, 74611, 40899)
+test_df = new_segments %>%
+  filter(local_id %in% test_ids) # n = 7
+
+# need to create new segment_length
+test_df$segment_length = as.numeric(sf::st_length(test_df))
+
+# calculate total segment length per local_id
+test_df = test_df %>%
+  group_by(local_id) %>%
+  mutate(total_segment_length = sum(segment_length)) %>%
+  ungroup()
+
+# obtain proportion of total segment length for each individual segment
+test_df$length_prop = test_df$segment_length / test_df$total_segment_length
+
+# calculate m cycled per working day
+test_df$m_cycled_per_working_day = round((test_df$length_prop * test_df$bicycle))
+
+
+
+
+
+
+sum(test_df$segment_length)/2 
+sum(test_df$New_segment_length)
+
+mapview(test_df,  zcol = "BOROUGH") + 
+  mapview(lon_lad_2020, alpha.regions = 0.1, zcol = "BOROUGH", legend = FALSE, lwd = 1)
+
+mapview(test_df)
+
+mapview(lon_rnet_intersection$geometry)
+plot(lon_rnet_intersection)
+
+borough_count = lon_rnet_intersection %>%
+  st_drop_geometry()%>%
+  group_by(BOROUGH) %>%
+  summarise(count = n())
+
+# #Calculate road length
+# lon_rnet$segment_length = as.numeric(sf::st_length(lon_rnet))
+# 
+# #summary(lon_rnet$segment_length)
+# # Min.   1st Qu.    Median      Mean   3rd Qu.      Max. 
+# # 0.145    44.753   103.081   170.911   207.936 11316.022 
+# 
+# # summary(lon_rnet$bicycle)
+# # Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+# # 0.00    1.00    6.00   53.33   25.00 4113.00 
+# 
+# #Calculate daily km's cycled
+# # calculate daily metres cycled per day (multiple segment length by number of users of that segment)
+# lon_rnet$m_cycled_per_working_day = lon_rnet$segment_length * lon_rnet$bicycle
+# 
+# # calculate using centroid, n = 74624  # BUT IT HAS PCT DATA FOR OUTSIDE LONDONO BOROUGHS 
+# lon_rnet_centroid = st_centroid(lon_rnet)  # check works - see if any difference with interseciton 
+# sum(lon_rnet_centroid$segment_length) # [1] 12754025
+# mapview(lon_rnet, color = "red") + mapview(lon_lad_2020, zcol = "BOROUGH") # shows the orginal pct data
+# mapview(lon_rnet_centroid)+ mapview(lon_lad_2020, zcol = "BOROUGH") # shows the centroids
+# 
+
+
+# Therefore would be better to limite rnet to lonond boundaries before doing anything.  
+# Limit rnet to London Borough boundaries
+lon_rnet2 = pct::get_pct_rnet(region = "london") # n = 74624
+lon_rnet2 = st_transform(lon_rnet2, crs=27700) 
+lon_rnet_intersection = st_intersection(x = lon_rnet2, y = lon_lad_2020) # n = 71494 
+mapview(lon_rnet_intersection, color = "red") + mapview(lon_lad_2020, zcol = "BOROUGH") # now pct data is limited to london boundaries
+
+city_rnet_intersection = lon_rnet_intersection %>%
+  filter(BOROUGH == "City of London")
+
+mapview(city_rnet_intersection)
+
+city_rnet_intersection$segment_length = as.numeric(sf::st_length(city_rnet_intersection)) # n = 336
+sum(city_rnet_intersection$segment_length) # n = 36841.98
+sum(city_rnet_intersection$bicycle) # n = 195272
+
+city_rnet_intersection$m_cycled_per_working_day = city_rnet_intersection$segment_length * city_rnet_intersection$bicycle
+sum(city_rnet_intersection$m_cycled_per_working_day)
+
+# Aggregate cycling to Borough level
+cycled_m_per_Borough_centroid = aggregate(lon_rnet_centroid["m_cycled_per_working_day"], lon_lad_2020, FUN = sum)
+
+# join to borough details to get borough labels
+cycled_m_per_Borough_centroid_labelled = st_join(cycled_m_per_Borough_centroid, lon_lad_2020) %>%
+  st_drop_geometry() %>%
+  group_by(BOROUGH)
+
+
+
+# OBtain million kms cycled for commuting per year (200 = number of working days)
+lon_lad_2020$millionkm_cycled_for_commuting_per_year_estimated = cycled_m_per_Borough$m_cycled_per_working_day * 
+  2 * 200 / # estimate of trips days per year, morning and afternoon
+  1e9 # to get million km
 
 # Convert route network to centroids
 #AGGREGATE USING STCENTROID ????
@@ -54,18 +268,21 @@ lon_rnet_centroid = st_centroid(lon_rnet)  # check works - see if any difference
 
 
 
-# Aggregate cycling to Borough level
-cycled_m_per_Borough = aggregate(lon_rnet["m_cycled_per_working_day"], lon_lad_2020, FUN = sum)
-
-# OBtain million kms cycled for commuting per year (200 = number of working days)
-lon_lad_2020$millionkm_cycled_for_commuting_per_year_estimated = cycled_m_per_Borough$m_cycled_per_working_day * 
-  2 * 200 / # estimate of trips days per year, morning and afternoon
-  1e9 # to get million km
 
 # Create dataset of Boroughs and estimated cycling
 Borough_commuting = lon_lad_2020 %>%
   st_drop_geometry() %>%
   select(c(BOROUGH, millionkm_cycled_for_commuting_per_year_estimated))
 
+
+
+
+
 # Save RDS
 # saveRDS(Borough_commuting, file = "/home/bananafan/Documents/PhD/Paper1/data/Borough_commuting")
+
+
+# import Robins split file
+robin_split = readRDS(file = "/home/bananafan/Downloads/rnet_split.Rds")
+names(robin_split)
+unique(robin_split$cat)
--- src/visualise_all_assets.R
+++ src/visualise_all_assets.R
@@ -10,6 +10,14 @@
 # - fonts - Arial (or Helvetica), Courier, Symbol, Times (or Times New Roman)
 # - Double column (full width) 	190 mm (539 pt) 	2244 	3740 	7480
 
+# Outstanding actions/ CURRENT STATUS as of 29 March 2021
+# – have legend built, 
+# just need to sort out how this looks with the overall map - 
+#   ? Make the plot margins for the overall map bigger then inset the legend and wording inside?  
+#   NEED to also do transparency thing 
+
+
+
 
 # install packages
 library(tidyverse)
@@ -17,6 +25,8 @@ library(sf)
 library(mapview)
 library(ggspatial) # get north arrow and bar
 library(patchwork) # arrange ggplots
+library(ggpubr) # for text grobs
+#library(ggsflabel) # makes sure ggplot geom_sf labels dont overlap using label_repel function
 #library(leafsync)
 # library(leaflet)
 # library(leafem)
@@ -48,47 +58,65 @@ c_crossings_point = st_centroid(c_crossings)
 c_signals_point = st_centroid(c_signals)
 c_trafficcalming = st_centroid(c_trafficcalming)
 
-# import May 2020 ONS LA boundary data (required for NA management)
-#lon_lad_2020 = readRDS(file = "./map_data/lon_LAD_boundaries_May_2020_BFE.Rds")
 
 # Create context for map
+
+# 1) Borough boundaries and labelling
 boroughs <- st_read("/home/bananafan/Documents/PhD/Paper1/map_data/London_Borough_Excluding_MHW.shp")
 borough_areas <- rmapshaper::ms_simplify(boroughs, keep=0.015) #Simplify boroughs
 borough_areas = rename(boroughs, BOROUGH = NAME)
-borough_areas$SHORT = fct_recode(borough_areas$BOROUGH, 
-                                    "KNS" = "Kensington and Chelsea",
-                                    "BAR" = "Barking and Dagenham",
-                                    "HMS" = "Hammersmith and Fulham",
-                                    "KNG" = "Kingston upon Thames",
-                                    "RCH" = "Richmond upon Thames",
-                                    "CTY" = "City of London",
-                                    "WTH" = "Waltham Forest",
-                                    "CRD" = "Croydon",
-                                    "BRM" = "Bromley",
-                                    "HNS" = "Hounslow",
-                                    "ELG" = "Ealing",
-                                    "HVG" = "Havering",
-                                    "HDN" = "Hillingdon",
-                                    "HRW" = "Harrow",
-                                    "BRT" = "Brent",
-                                    "BRN" = "Barnet",
-                                    "LAM" = "Lambeth",
-                                    "SWR" = "Southwark", 
-                                    "LSH" = "Lewisham",
-                                    "GRN" = "Greenwich",
-                                    "BXL" = "Bexley",
-                                    "ENF" = "Enfield",
-                                    "RDB" = "Redbridge",
-                                    "STN" = "Sutton",
-                                    "MRT" = "Merton",
-                                    "WNS" = "Wandsworth",
-                                    "WST" = "Westminster",
-                                    "CMD" = "Camden",
-                                    "TOW" = "Tower Hamlets",
-                                    "ISL" = "Islington",
-                                    "HCK" = "Hackney",
-                                    "HGY" = "Haringey",
-                                    "NWM" = "Newham")
+
+borough_areas$B_numbered = fct_recode(borough_areas$BOROUGH, 
+                                 "7" = "Kensington and Chelsea",
+                                 "32" = "Barking and Dagenham",
+                                 "8" = "Hammersmith and Fulham",
+                                 "25" = "Kingston upon Thames",
+                                 "24" = "Richmond upon Thames",
+                                 "1" = "City of London",
+                                 "15" = "Waltham Forest",
+                                 "28" = "Croydon",
+                                 "29" = "Bromley",
+                                 "23" = "Hounslow",
+                                 "20" = "Ealing",
+                                 "31" = "Havering",
+                                 "22" = "Hillingdon",
+                                 "21" = "Harrow",
+                                 "19" = "Brent",
+                                 "18" = "Barnet",
+                                 "10" = "Lambeth",
+                                 "11" = "Southwark", 
+                                 "12" = "Lewisham",
+                                 "13" = "Greenwich",
+                                 "30" = "Bexley",
+                                 "17" = "Enfield",
+                                 "33" = "Redbridge",
+                                 "27" = "Sutton",
+                                 "26" = "Merton",
+                                 "9" = "Wandsworth",
+                                 "6" = "Westminster",
+                                 "5" = "Camden",
+                                 "2" = "Tower Hamlets",
+                                 "4" = "Islington",
+                                 "3" = "Hackney",
+                                 "16" = "Haringey",
+                                 "14" = "Newham")
+
+
+
+# Create Inner London Borough list
+inn_lon_B_list = c("City of London", "Camden", "Greenwich", "Hackney", "Hammersmith & Fulham", 
+                   "Islington", "Kensington & Chelsea", "Lambeth", "Lewisham", "Southwark",  
+                   "Tower Hamlets", "Wandsworth", "Westminster") 
+# Create inner London spatial object for boundary
+inn_lon_union = borough_areas %>%
+  filter(BOROUGH %in% inn_lon_B_list) %>%
+  st_union()
+# Create Outer London Spatial object for boundary
+out_lon_union = borough_areas %>%
+  filter(!BOROUGH %in% inn_lon_B_list) %>%
+  st_union()
+
+
 ### using short names in tmap
 # mapped_boroughs1 = tm_shape(mapping_boroughs) +
 #   tm_fill(col = "ivory2") +
@@ -96,40 +124,88 @@ borough_areas$SHORT = fct_recode(borough_areas$BOROUGH,
 #   tm_text("SHORT", size = 0.7) +
 #   tm_layout(bg.color = "lightblue")
 
+# 2) River thames
 riverthames = st_read("/home/bananafan/Documents/PhD/Paper1/map_data/riverthames.shp")
 riverthames_simplify = rmapshaper::ms_simplify(riverthames)
 
+# 3) Motorways
 motorways <- st_read("/home/bananafan/Documents/PhD/Paper1/map_data/motorways_outer.json") %>% 
   st_transform(crs=27700) 
 box_new = c(xmin = 498745.5, ymin = 149044.6, xmax = 564000.0, ymax = 205391.0)
 motorways = st_crop(motorways, box_new)
 #box_orig = c(xmin = 498745.5, ymin = 149044.6, xmax = 569602.4, ymax = 205391.0)
 
+# 4) Create map legend
+# create legend text
+legend_text =  data.frame(
+  lineend = c("River", "Inner London", "Outer London", "Motorway"),
+  linejoin = c("Thames", "Boundary", "Boundary", "Network"))
 
+# create legend dataframe
+df = data.frame(legend_text, y = 1:4)
 
+# create ggplot for map legend
+legend = ggplot(df, aes(x = 1, y = y, xend = 1.5, yend = y, label = paste(lineend, linejoin))) +
+  geom_segment(size = 3, colour = c("#99CCEE", "#991100", "black", "#b77107")) +
+  geom_text(hjust = 'outside', nudge_x = -0.25) +
+  xlim(0.5, 1.5) +
+  theme_void() +
+  theme(plot.margin = unit(c(0.25, 0.25, 0.25, -7.5), "cm"))
+
+# 5) Create borough legend
+text1 = paste("1 City of London", "2 Tower Hamlets", "3 Hackney",
+              "4 Islington", "5 Camden", "6 Westminster",
+              "7 Kensington and Chelsea", "8 Hammersmith and Fulham",
+              "9 Wandsworth", "10 Lambeth", "11 Southwark", 
+              "12 Lewisham", "13 Greenwich", "14 Newham",
+              "15 Waltham Forest", "16 Haringey", sep = "\n")
+
+text2 = paste("17 Enfield", "18 Barnet", "19 Brent", "20 Ealing", "21 Harrow", 
+              "22 Hillingdon", "23 Hounslow", "24 Richmond upon Thames",
+              "25 Kingston upon Thames", "26 Merton", "27 Sutton", 
+              "28 Croydon", "29 Bromley", "30 Bexley", "31 Havering", 
+              "32 Barking and Dagenham", "33 Redbridge", sep = "\n")
+
+# Create text grobs
+B1_grob = text_grob(text1, just = "left")    
+B2_grob = text_grob(text2, just = "left")   
+
+# Convert text grobs to ggplot objects
+B1_ggplot = as_ggplot(B1_grob) + 
+  theme(plot.margin = unit(c(0.5, -1, -1, -12), "cm"))
+
+B2_ggplot = as_ggplot(B2_grob) +
+  theme(plot.margin = unit(c(0.5, -1, 1, -12), "cm"))
+
+# 6) Create overall legend
+final_legend = legend / (B1_ggplot | B2_ggplot)  ##### NEEDS TIDYING
 
 
 
 
 # Create base maps
  # with and without scales and arrows
-baseplot_arrow_scale = ggplot()+
-  geom_sf(data=motorways, fill="#EEEEEE",  colour="#EEEEEE")+
-  geom_sf(data=borough_areas, fill="#d4d4d4",  colour="#444444", alpha=0.3, size=0.05)+
+basemap = ggplot()+
+  geom_sf(data = out_lon_union, fill="white", colour = "black") +
+  geom_sf(data=motorways, colour = "#b77107", size = 0.14)+
+  geom_sf(data = inn_lon_union, colour = "#991100") +
+  geom_sf(data=borough_areas, fill="#d4d4d4",  colour="black", alpha=0.3, size=0.15)+
   geom_sf(data=riverthames_simplify, fill="#99CCEE",  colour="#99CCEE")+
+  geom_sf_label(data = borough_areas, aes(label = B_numbered)) +
   theme_bw() +
   coord_sf(crs=st_crs(riverthames_simplify), datum=NA) +
+  xlab(label = NULL) +
+  ylab(label = NULL) +
   annotation_scale(location = "br", width_hint = 0.3, bar_cols = c("Gray83", "white"),
                    text_cex = 0.5, line_width = 0.5, line_col = "#222222") +
   annotation_north_arrow(location = "tr", which_north = "true", 
                          height = unit(1.3, "cm"), width = unit(1.3, "cm"),
-                         #pad_x = unit(0.75, "in"), pad_y = unit(0.5, "in"),
                          style = north_arrow_fancy_orienteering(line_width = 0.5, 
                                                                 line_col = "#222222",
                                                                 fill = c("white", "Gray83"),
                                                                 text_size = 8))
 
-baseplot = ggplot()+
+basemap_noArrow = ggplot()+
   geom_sf(data=motorways, fill="#EEEEEE",  colour="#EEEEEE")+
   geom_sf(data=borough_areas, fill="#d4d4d4",  colour="#444444", alpha=0.3, size=0.05)+
   geom_sf(data=riverthames_simplify, fill="#99CCEE",  colour="#99CCEE")+
@@ -212,6 +288,7 @@ p5 = ggplot()+
 # Use patchwork to create plot
 locations_plot = (p1 | p2 | p3) / (p4 | p5)
 
+test = (pm | p1|p2) / (p3 | p4 | p5)
 
 #### CREATE NEW EAXMPLE WITH TRANSPARENCY _ below code alters transpoarent
 geom_sf(data = c_signals_point, colour = alpha("red", 0.2), size = 0.1) +
@@ -241,4 +318,69 @@ geom_sf(data = c_signals_point, colour = alpha("red", 0.2), size = 0.1) +
 # m2 = mapview(rt_simplify)
 # leafsync::sync(m1,m2)
 # st_write(riverthames, "/home/bananafan/Documents/PhD/Paper1/map_data/riverthames.shp")
+
+
+
+# Legend and other map work
+# borough_text = paste("1 City of London", "2 Tower Hamlets", "3 Hackney",
+#                      "4 Islington", "5 Camden", "6 Westminster",
+#                      "7 Kensington and Chelsea", "8 Hammersmith and Fulham",
+#                      "9 Wandsworth", "10 Lambeth", "11 Southwark", 
+#                      "12 Lewisham", "13 Greenwich", "14 Newham",
+#                      "15 Waltham Forest", "16 Haringey", "17 Enfield", 
+#                      "18 Barnet", "19 Brent", "20 Ealing", "21 Harrow", 
+#                      "22 Hillingdon", "23 Hounslow", "24 Richmond upon Thames",
+#                      "25 Kingston upon Thames", "26 Merton", "27 Sutton", 
+#                      "28 Croydon", "29 Bromley", "30 Bexley", "31 Havering", 
+#                      "32 Barking and Dagenham", " 33 Redbridge", sep = "\n")
+
+
+
+# df = data.frame(ma = rep(c("Motorway network", "Outer London Boundary", "Inner London Boundary", "River Thames"), each = 4),
+#   x1 = rep(c(1,2), each =  8),
+#   #x2 = rep(2, each = 8),
+#   y1 = rep(seq(1:8), each = 2))
+# 
+# df2 = data.frame(ma = rep("Motorway network" , each = 4),
+#                 x1 = c(1,2, 1,2),
+#                 #x2 = rep(2, each = 8),
+#                 y1 = c(1,1,2,2))
+# 
+# df3 = data.frame(ma = rep(c("Motorway network", "Outer London Boundary", "Inner London Boundary", "River Thames"), each = 1),
+#                  x1 = rep(1, each = 1),
+#                  x2 = rep(2, each = 1),
+#                  y1 = c(1,2,3,4),
+#                  y2 = c(1,2,3,4))
+#                  
+# 
+# 
+# 
+# x2 = rep(2, each = 4),
+#                  y1 = 1,
+#                  y2 = 1)
+# 
+# 
+# ggplot()+
+#   geom_segment(data = df3, aes(x = x1, y = y1, xend = x2, yend = y2),
+#                colour = c("#99CCEE", "#991100", "black", "#b77107"),
+#                size = 4, 
+#                labels = ma)
+# 
+# legend_names = (c("Motorway network", "Outer London Boundary", "Inner London Boundary", 
+#                    "River Thames"))
+# 
+# 
+# 
+# legend_lines = ggplot(data = df2, aes(x = x1, y = y1, group = ma)) +
+#   geom_point(alpha = 0) +
+#   geom_hline(yintercept = 1, colour="#99CCEE", size = 2) +
+#   geom_hline(yintercept = 2, colour = "#991100", size = 2) +
+#   geom_hline(yintercept = 3, colour = "black", size = 2) +
+#   geom_hline(yintercept = 4, color = "#b77107", size = 2) +
+#   theme_void()
+
+
+# cod
+
+
 
